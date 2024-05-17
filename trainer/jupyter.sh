e6ec0e7ad3a506429083defa3348c91a7418486f
ajmalrasi <kpajmalrasi@gmail.com>
2024-05-17 08:10:04 +0530

    Sub module

--- trainer/jupyter.sh
+++ trainer/jupyter.sh
@@ -4,8 +4,8 @@
 if [ -z "$1" ]
   then
     echo "Starting Jupyter Notebook on the default port (8888)"
-    jupyter lab --ip 0.0.0.0 --no-browser --allow-root 
+    jupyter lab --ip 0.0.0.0 --no-browser --allow-root --LabApp.token=''
   else
     echo "Starting Jupyter Notebook on port: $1"
-    jupyter lab --ip 0.0.0.0 --no-browser --allow-root --port=$1
+    jupyter lab --ip 0.0.0.0 --no-browser --allow-root --LabApp.token='' --port=$1
 fi
--- trainer/tensorboard.sh
+++ trainer/tensorboard.sh
@@ -4,8 +4,8 @@
 if [ -z "$1" ]
   then
     echo "Starting TensorBoard on the default port (6006)"
-    tensorboard --logdir "/home/ajmalrasi/hiba/train/" --host 0.0.0.0 --port 6006
+    tensorboard --logdir "/home/ajmalrasi/car_detection_tf1/models" --host 0.0.0.0 --port 6006
   else
     echo "Starting TensorBoard on port: $1"
-    tensorboard --logdir "/home/ajmalrasi/hiba/train/" --host 0.0.0.0 --port=$1
+    tensorboard --logdir "/home/ajmalrasi/car_detection_tf1/models" --host 0.0.0.0 --port=$1
 fi
